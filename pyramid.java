90d45f513b5fb44b87d62f9cbcf2d401f1ed623d
kkellyin90 <keith.kelly.a@gmail.com>
2016-05-05 19:45:52 +0530

    Added files via upload

---
+++ pyramid.java
@@ -0,0 +1,24 @@
+
+public class pyramid {
+
+	public static void main(String[] args) {
+		
+		int counter = 0;
+		while (counter ==0 && counter <=4){
+			System.out.println("1");
+			counter++;//1 count
+			System.out.println("\n12");
+			counter++;//2 count 
+			System.out.println("\n123");
+			counter++;//3 count 
+			System.out.println("\n1234");
+			counter++;//4 count
+			System.out.println("\n12345");
+		}
+		System.out.println("\n1234");
+		System.out.println("\n123");
+		System.out.println("\n12");
+		System.out.println("\n1");
+	}
+
+}
